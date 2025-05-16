41f95fd6edec742dd6782357e508968248952bd6
xdguido <guidogennari95@gmail.com>
2025-05-16 01:40:12 -0300

    fix: mobile

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-background text-foreground">
+    <div className="flex justify-center min-h-screen bg-background text-foreground">
       {/* Grid container with max-width and consistent horizontal margins */}
-      <div className="max-w-7xl border border-border mx-auto">
+      <div className="max-w-7xl border border-border mx-4">
         {/* Header */}
         <header className="grid grid-cols-12 gap-4 py-6 px-4 sm:px-6 lg:px-8">
-          <div className="col-span-6 md:col-span-6 flex items-center">
+          <div className="col-span-6 flex items-center">
             <div className="flex items-center gap-2">
               <span className="font-bold tracking-tight">XD.STUDIO</span>
             </div>
@@ -33,7 +33,7 @@ export default function Home() {
         <main>
           {/* Hero Section */}
           <section className="grid grid-cols-12 gap-4 pt-16 pb-24 px-4 sm:px-6 lg:px-8">
-            <div className="col-span-7 flex flex-col items-end justify-center text-right">
+            <div className="col-span-12 xl:col-span-7 flex flex-col items-center xl:items-end justify-center text-center xl:text-right">
               <h1 className="text-5xl md:text-7xl font-bold tracking-tighter mb-6">
                 Que quieres <br />{' '}
                 <span className="text-primary">construir</span> hoy?
@@ -49,7 +49,7 @@ export default function Home() {
                 Comenzar <ArrowRight className="ml-1 h-5 w-5" />
               </Button>
             </div>
-            <div className="col-span-5 -mt-6">
+            <div className="hidden xl:block col-span-5 -mt-6">
               <LogoParticles />
             </div>
           </section>
@@ -59,7 +59,7 @@ export default function Home() {
             id="services"
             className="grid grid-cols-12 border-t border-b gap-4 py-24 px-4 sm:px-6 lg:px-8"
           >
-            <div className="col-span-4 pt-4 text-center">
+            <div className="col-span-12 md:col-span-4 p-8 md:text-center">
               <h2 className="text-3xl md:text-4xl font-bold tracking-tighter">
                 Nuestros Servicios
               </h2>
@@ -111,7 +111,10 @@ export default function Home() {
           </section>
 
           {/* Contact Section */}
-          <section id="contact" className="grid grid-cols-12 gap-4 py-24">
+          <section
+            id="contact"
+            className="grid grid-cols-12 gap-4 py-24 px-4 sm:px-6 lg:px-8"
+          >
             <div className="col-span-12 lg:col-start-3 lg:col-span-8 text-center">
               <h2 className="text-3xl md:text-5xl font-bold tracking-tighter mb-6">
                 Da un paso adelante con tu proyecto
